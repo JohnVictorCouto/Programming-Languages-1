9fdc8262e2636bef1b71f7a236dafb9b3be7d03a
João Victor Pereira Couto <jvpcouto@hotmail.com>
2026-03-31 12:42:23 -0300

    Delete Aula 6 directory

--- Aula 6/ex1/Teste.java
+++
@@ -1,9 +0,0 @@
-public class Teste {
-    public static void main(String[] args) {
-        Relogio relogio = new Relogio(); //
-        for (int i = 0; i < 1440; i++) { // simula o avanço do relógio por um dia inteiro (1440 minutos)
-            relogio.ticTac(); // avança o relógio em um minuto
-            System.out.println(relogio.mostra()); // imprime a hora atual no formato "HH:MM" a cada minuto
-        }
-    }
-}
